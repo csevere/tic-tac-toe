6480e56a87386bf6d9bb3feb20c2b2dcb68b2536
Carlita <carla.severe@gmail.com>
2017-06-27 01:10:32 -0400

    gave square component a state, can now put x in boxes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 //show the value passed in from the board renderSquare component 
+//make the Square component fill in an "X" when you click it
+//change button tag returneed in render function of Square
+//Passing a function as the onClick prop
+//Doing onClick={alert('click')} would alert immediately instead of when the button is clicked.
+
+//React components can have state by setting this.state in the constructor, 
+//which should be considered private to the component. 
+//Let's store the current value of the square in state, 
+//and change it when the square is clicked.
+
+//In JavaScript classes, you need to explicitly call super(); 
+//when defining the constructor of a subclass.
+
+//Now change the Square render method to 
+//display the value from the current state, and to toggle it on click:
+
+ // <button className="square" onClick = {() => alert('click')}>
+      //   {this.props.value}
+      // </button>
+
+//Replace this.props.value with this.state.value 
 
 class Square extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      value: null, 
+
+    };
+    
+  }
+
   render() {
+    
     return (
-      <button className="square">
-   
-        {this.props.value}
+     
+      <button className="square" onClick = {() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
+
+
     );
   }
 }
+
 
 
 class Board extends React.Component {
